01d27044e0a3aa93fb3fc826c4f73f1daa2029c1
Michael Becker <21beckem8@gmail.com>
2025-07-10 13:01:47 -0500

    Moved sidebar and navbar stuff to it's own script to be the same on every page

---
+++ web/every-page.js
@@ -0,0 +1,45 @@
+// settings
+const showDateCol = localStorage.getItem('KinexWorkDay_showDateCol') == 'true';
+const Kinex_Role = localStorage.getItem('KinexWorkDay_role') || 'Cleaner';
+
+// fill in navbar
+if (document.querySelector('navbar')) {
+    document.querySelector('navbar').innerHTML = `
+        <button>
+            <hamburger style="margin-right: 15px;"></hamburger> <span>${Kinex_Role}</span>
+        </button>
+        <img src="https://mma.prnewswire.com/media/1730057/Kinex_Logo.jpg?p=twitter" alt="Kinex Logo">
+    `;
+}
+
+// fill in sidebar
+if (document.querySelector('sidebar-container')) {
+    document.querySelector('sidebar-container').innerHTML = `
+        <deselect></deselect>
+        <sidebar>
+            <img src="https://mma.prnewswire.com/media/1730057/Kinex_Logo.jpg?p=twitter" alt="Kinex Logo">
+            <h2>Pages</h2>
+                <button onclick="location.href = 'index.html'"><h3>Go Home</h3></button>
+            <h2>Options</h2>
+                <button class="setRole" role="Cleaner"><h3>Enter Cleaner Role</h3></button>
+                <button class="setRole" role="Repairer"><h3>Enter Repairer Role</h3></button>
+                <button class="toggleDateCol"><h3>Toggle Date Column</h3></button>
+        </sidebar>
+    `;
+    // event listeners
+    document.querySelector('deselect').onclick = () => {
+        document.querySelector('sidebar-container').classList.remove('active');
+    }
+    document.querySelector('button:has(hamburger)').onclick = () => {
+        document.querySelector('sidebar-container').classList.add('active');
+    }
+    document.querySelectorAll('sidebar button.setRole').forEach(x => x.onclick = () => {
+        document.querySelector('sidebar-container').classList.remove('active');
+        localStorage.setItem('KinexWorkDay_role', x.getAttribute('role'));
+        location.reload();
+    });
+    document.querySelector('sidebar button.toggleDateCol').onclick = () => {
+        localStorage.setItem('KinexWorkDay_showDateCol', !showDateCol);
+        location.reload();
+    }
+}
